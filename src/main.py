ae29c580cdc80e45df58dbbee21ca3a53c4bb673
rccyamazaki0129 <rccyamazaki0129@gmail.com>
2021-03-15 10:34:22 +0900

    info update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import datetime
 import xlwings as xw
+import pandas as pd
 
 infoPath = '../list/info.xlsx'
 templatePath = '../list/template.xlsx'
@@ -10,8 +11,12 @@ d_month = d_now.month + 1
 title = str(d_month) + '月分請求書'
 
 # Create new Excel file
-App = xw.App()
-wb = xw.Book()
+# App = xw.App()
+# wb = xw.Book()
 
-xw.Range('A1').value = 'TEST'
-wb.save('test02.xlsx')
+# Write data into Excel file
+# xw.Range('A1').value = title
+# wb.save('test.xlsx')
+
+df = pd.read_excel(infoPath)
+print("read_excel succeeded")
