095e9403b99737f1697c14d3fe97ed64e70b04b7
rccyamazaki0129 <rccyamazaki0129@gmail.com>
2021-03-23 11:12:24 +0900

    latest version

--- src/main.py
+++ src/main.py
@@ -6,15 +6,25 @@ import pandas as pd
 import os
 
 #プログラム2｜対象エクセルのファイルパスを指定
-samplepath = '../list/info.xlsx'
-templatepath = '../list/template.xlsx'
-savepath = '../bill/'
+samplepath = 'list/info.xlsx'
+templatepath = 'list/template.xlsx'
+#savepath has to be absolute path to use in ExportAsFixedFormat func
+# savepath = 'C:/Yamazaki_Rei/misc/BillGenerator/bill/'
 
 #プログラム3｜エクセルを読み込み、日付を変換
 df = pd.read_excel(samplepath)
 dt_now = datetime.datetime.now()
 title = str(dt_now.month + 1) + '月分請求書'
+year = str(dt_now.year) + '年_'
+fileTitle = year + title
 # df['納品日'] = pd.to_datetime(df['納品日']).dt.strftime("%Y-%m-%d")
+
+# Create directories for saving files
+current_path = os.getcwd()
+new_folder_path = current_path + '/bill/' + fileTitle
+new_excel_folder_path = new_folder_path + '/excel/'
+os.mkdir(new_folder_path)
+os.mkdir(new_excel_folder_path)
 
 #プログラム4｜取引先のリストを作成
 torihiki_list = sorted(list(df['生徒氏名'].unique()))
@@ -46,6 +56,10 @@ for torihiki in torihiki_list:
     for rows in values:
         print(rows[0])
         ws.range('A1').value = title
+        ws.range('J5').value = dt_now.strftime('%Y年%m月%d日')
+        reiwa = str(dt_now.year - 2018)
+        ws.range('J6').value = reiwa
+        ws.range('K6').value = dt_now.month
         ws.range('A2').value = rows[5]
         ws.range('A3').value = rows[6]
         ws.range('A6').value = rows[0]
@@ -101,11 +115,14 @@ for torihiki in torihiki_list:
         ws.range('A32').value = rows[11]
 
     filename = torihiki + '.xlsx'
-    pdf_path = os.path.join(savepath, f'{torihiki}_report.pdf')
-    # wb.api.ExportAsFixedFormat(0, pdf_path)
-    # os.startfile(pdf_path)
+    # savepath = ws.range('P1').value + '/'
+    # saveXlsxPath = ws.range('P2').value + '/'
 
-    wb.save('../bill/' + filename)
+    pdf_path = os.path.join(new_folder_path, f'{torihiki}_reportXX.pdf')
+    wb.api.ExportAsFixedFormat(0, pdf_path)
+    os.startfile(pdf_path)
+
+    wb.save(new_excel_folder_path + filename)
     wb.close()
 
 App.quit()
